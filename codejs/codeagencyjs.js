9cb6234089d1776e6ee8614f884782d813485adb
Mr.Sourav Ghosh <ghsourav@github.com>
2020-03-05 12:40:54 +0530

    Responsive Webtemplate

---
+++ codejs/codeagencyjs.js
@@ -0,0 +1,37 @@
+
+
+let time = document.getElementById('copy');
+
+function clock() {
+    let date = new Date();//Date Object
+    let year = date.getFullYear();
+    time.innerHTML = `<p>&copy; All Rights Reseved ${year}.Develop By <a href="https://twitter.com/ghsourav" target="_blank">Ghsourav</a>  </p>`
+
+
+
+
+
+
+}
+
+clock();
+
+
+
+
+
+
+
+
+
+
+
+
+$(".jumbotron").css({ height: $(window).height() + "px" });
+
+$(window).on("resize", function() {
+  $(".jumbotron").css({ height: $(window).height() + "px" });
+});
+
+
+
---
+++ codejs/scroll.js
@@ -0,0 +1,32 @@
+
+// scroll revel
+
+window.sr=ScrollReveal();
+
+sr.reveal('.animate-left',{
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+
+sr.reveal('.animate-right',{
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-top',{
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-bottom',{
+    origin: 'bottom',
+    duration: 1300,
+    distance: '25rem',
+    delay: 400
+});
